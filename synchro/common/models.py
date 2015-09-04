7c586a09b2c1f1a4cd2cdef85ea895002eacbd36
Tyler Vey <tylervey@gmail.com>
2015-09-03 22:33:23 -0230

    people index, started about page, added javascript

--- synchro/common/models.py
+++ synchro/common/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, \
-    PageChooserPanel
+    PageChooserPanel, InlinePanel
 from wagtail.wagtaildocs.edit_handlers import DocumentChooserPanel
 from wagtail.wagtailsnippets.models import register_snippet
+from wagtail.wagtailsearch import index
 
 from modelcluster.fields import ParentalKey
 
@@ -110,3 +113,73 @@ class Advert(models.Model):
         return self.text
 
 register_snippet(Advert)
+
+
+# Standard index page
+
+class StandardIndexPageRelatedLink(Orderable, RelatedLink):
+    page = ParentalKey('common.StandardIndexPage', related_name='related_links')
+
+
+class StandardIndexPage(Page):
+    intro = RichTextField(blank=True)
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    search_fields = Page.search_fields + (
+        index.SearchField('intro'),
+    )
+
+StandardIndexPage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('intro', classname="full"),
+    InlinePanel(StandardIndexPage, 'related_links', label="Related links"),
+]
+
+StandardIndexPage.promote_panels = Page.promote_panels + [
+    ImageChooserPanel('feed_image'),
+]
+
+
+# Standard page
+
+class StandardPageCarouselItem(Orderable, CarouselItem):
+    page = ParentalKey('common.StandardPage', related_name='carousel_items')
+
+
+class StandardPageRelatedLink(Orderable, RelatedLink):
+    page = ParentalKey('common.StandardPage', related_name='related_links')
+
+
+class StandardPage(Page):
+    intro = RichTextField(blank=True)
+    body = RichTextField(blank=True)
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    search_fields = Page.search_fields + (
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    )
+
+StandardPage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('intro', classname="full"),
+    InlinePanel(StandardPage, 'carousel_items', label="Carousel items"),
+    FieldPanel('body', classname="full"),
+    InlinePanel(StandardPage, 'related_links', label="Related links"),
+]
+
+StandardPage.promote_panels = Page.promote_panels + [
+    ImageChooserPanel('feed_image'),
+]
--- synchro/common/templatetags/common_tags.py
+++ synchro/common/templatetags/common_tags.py
@@ -76,3 +76,18 @@ def breadcrumbs(context):
         'ancestors': ancestors,
         'request': context['request'],
     }
+
+
+# Retrieves all live pages which are children of the calling page
+#for standard index listing
+@register.inclusion_tag(
+    'common/tags/standard_index_listing.html',
+    takes_context=True
+)
+def standard_index_listing(context, calling_page):
+    pages = calling_page.get_children().live()
+    return {
+        'pages': pages,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
