711d9414340eadd1e1fc8a9cf35ebb1e0b8cf394
root <user.name>
2015-09-12 14:40:27 -0400

    Set basic production information, not functional yet

--- synchro/manage.py
+++ synchro/manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "synchro.settings")
+
+    if os.environ['IS_PRODUCTION']:
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "synchro.settings.production")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "synchro.settings")
 
     from django.core.management import execute_from_command_line
 
--- synchro/synchro/settings/production.py
+++ synchro/synchro/settings/production.py
@@ -1,9 +1,23 @@
 from .base import *
 
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+#DEBUG = False
+#TEMPLATE_DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+SECRET_KEY = os.environ['IS_PRODUCTION']
 
 try:
     from .local import *
