f5b33b7f6af0d620489633181bc09775e8a172c7
Secretly Evil <user.name>
2018-08-27 13:27:38 -0400

    serverside commits

--- synchro/gallery/models.py
+++ synchro/gallery/models.py
@@ -9,6 +9,8 @@ from wagtail.wagtailimages.models import AbstractImage
 from wagtail.wagtailsearch import index
 
 from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 
 # Abstract model for picking images
@@ -31,6 +33,10 @@ class SimpleImage(models.Model):
         abstract = True
 
 
+class GalleryPageTag(TaggedItemBase):
+    content_object = ParentalKey('GalleryPage', related_name='tagged_items')
+
+
 # Basic Gallery page with optional thumbnail
 class GalleryPage(Page):
     body = RichTextField(blank=True)
@@ -41,14 +47,34 @@ class GalleryPage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    image_tags = ClusterTaggableManager(through=GalleryPageTag, blank=True)
 
     search_fields = Page.search_fields + [
         index.SearchField('body'),
     ]
 
+
+    def get_images(self):
+        """ Get images with tag or manually added to gallery and return them sorted by creation date """
+
+        id_list = []
+        for simple_image in self.simpleimage.all():
+            id_list.append(simple_image.image.id)
+        manual_set = Image.objects.filter(id__in=id_list)
+
+        tag_set = Image.objects.none()
+        for tag in self.image_tags.all():
+            tag_set = tag_set.union(Image.objects.all().filter(tags__name=tag.name))
+
+        union_set = tag_set.union(manual_set)
+
+        return union_set.order_by('-created_at')
+
+
 GalleryPage.content_panels = [
     FieldPanel('title', classname="full title"),
     FieldPanel('body', classname="full"),
+    FieldPanel('image_tags', classname="full"),
     InlinePanel('simpleimage', label="Images")
 ]
 
